2ecf9696a72c2cce75fa0f3b96e36903a8b80e2d
UnknownFinder <zuzinma2009@mail.ru>
2025-08-07 23:54:37 +0300

    Update agent-47.sh

--- agent-47.sh
+++ agent-47.sh
@@ -20,60 +20,15 @@ if [ $choice -eq 1 ]; then
 	bash $SCRIPT_DIRECTORY/system_monitor.sh
 fi
 if [ $choice -eq 2 ]; then
-	while true; do
-    echo "Please read this instruction to avoid problems in future:"
-    echo "If you want to block one port enter 2, then follow the instructions"
-    echo "If you want to block more than one port enter 3, then follow the instruction"
-	echo "If you want to create white list of IP-addresses and block all orher connections enter 4, then follow the instruction"
-	echo "If you want to create blacklist of IP-addresses and accept all other connections entire 5 then follow the instruction"
-	echo "If you want to delete all chains, enter 6."
- 	echo "Enter 99 to exit."
-   	read firewall_choice
-    if [ $firewall_choice -eq 2 ]; then
-    	echo "You have chosen to close one port. Please, enter a port number:"
-    	read port
-        sudo iptables -A INPUT -p tcp --dport $port -j DROP
-        sudo iptables -A INPUT -p UDP -s 0/0 --dport $port -j DROP
-	fi
-    if [ $firewall_choice -eq 3 ]; then
-        echo "You have chosen to close some ports. Please, enter ports, which must be closed. Press 0 to stop."
-		while true; do
-  		read port
-  		if [[ $port -eq 0 ]]; then
-  			break
-	 	fi
-        sudo iptables -A INPUT -p tcp --dport $port -j DROP
-        sudo iptables -A OUTPUT -p tcp --dport $port -j DROP
-        sudo iptables -A INPUT -p UDP -s 0/0 --dport $port -j DROP
-        done
-	fi
-        if [ $firewall_choice -eq 4 ]; then
-            echo "Enter ip-addresses that must be added to whitelist:"
-            declare -a whitelist
-            read -a whitelist
-            for ip_address in "${whitelist}"; do
-                sudo iptables -A INPUT -s $ip_address -j ACCEPT
-                sudo iptables -A OUTPUT -s $ip_address -j ACCEPT
-            done
-		fi
-        if [ $firewall_choice -eq 5 ]; then 
-			echo "Enter ip-addresses which must be added at blacklist:"
-            declare -a blacklist
-            read -a blacklist
-            for ip_address in "${blacklist}"; do
-                sudo iptables -A INPUT -s $ip_address -j DROP
-                sudo iptables -A OUTPUT -s $ip_address -j DROP
-            done
-		fi
-  		if [ $firewall_choice -eq 6 ]; then
-            sudo iptables -F
-			sudo iptables -X
-   		fi
-	 	if [ $firewall_choice -eq 99 ]; then
-   			break
-	  	fi
-	echo "O'kay, you've create some firewall settings."
-	done
+	#We will drop all packages with invalid status
+    sudo iptables -A INNPUT -m state --state INVALID -j DROP
+    sudo iptables -A FORWARD -m state --state INVALID -j DROP
+    #Protecting from SYN flood
+    sudo iptables -A INPUT  -p tcp ! --syn -m state --state NEW -j DROP
+    sudo iptables -A OUTPUT -p tcp ! --syn -m state --state NEW -j DROP
+    #Protecting from ICMP redirectioin
+    sudo iptables -A INPUT --fragment -p ICMP -j DROP
+    sudo iptables -A OUTPUT --fragment -p ICMP -j DROP
 fi
 if [ $choice -eq 3 ]; then
 	echo "O'kay, my friend. Now enter:"
