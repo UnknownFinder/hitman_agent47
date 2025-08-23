bf94436c6a48c07e7ad32549fae372ad173f9aa6
UnknownFinder <zuzinma2009@mail.ru>
2025-08-23 12:25:34 +0300

    Update call_daemon.sh

--- call_daemon.sh
+++ call_daemon.sh
@@ -9,8 +9,8 @@ echo "After=network.service" >> $UNIT_FILE
 
 echo "[Service]" >> $UNIT_FILE
 echo "Type=exec" >> $UNIT_FILE
-echo "ExecStart=$EXEC_FILE" >> $UNIT_FILE
-echo "Restart=always" >> $UNIT_FILE
+echo "ExecStart="$SCRIPT_DIRECTORY/hitman2.sh $max_cpu $max_ram" >> $UNIT_FILE
+echo "Restart=on-failure" >> $UNIT_FILE
 
 echo "[Install]" >> $UNIT_FILE
 echo "WantedBy=multi-user.target" >> $UNIT_FILE
