b061e8144898fb69bab5ec158e8c98ed9ee89721
UnknownFinder <zuzinma2009@mail.ru>
2025-08-23 11:57:47 +0300

    Update hitman2.sh

--- hitman2.sh
+++ hitman2.sh
@@ -12,7 +12,7 @@ fi
 #Поиск процессов, превышающих установленные пороги
 high_cpu_proc=$(top -bn1 | grep PID -A 20 | tail -n +2 | awk -v tresh=$cpu_max '{if ($9+0>=tresh){print $3}}')
 high_ram_proc=$(top -bn1 | grep PID -A 20 | tail -n +2 | awk -v tresh=$ram_max '{if ($10+0>=tresh){print $3}}')
-if [ -n "$cpu_max" ] || [ -n "$ram_max" ]; then
+if [ -n "$high_cpu_proc" ] || [ -n "$high_ram_proc" ]; then
 	echo "Warning! Overloading is detected."
 	for pid in ${cpu_max[@]} ${ram_max[@]}; do
 		echo "Changing priority for process $pid"
@@ -26,7 +26,7 @@ ex_cpu=95
 ex_mem=95
 ex_cpu_proc=$(top -bn1 | grep PID -A 20 | tail -n +2 | awk -v tresh=$ex_cpu '{if ($9+0>=tresh){print $3}}')
 ex_mem_proc=$(top -bn1 | grep PID -A 20 | tail -n +2 | awk -v tresh=$ex_mem '{if ($10+0>=ex_mem){print $3}}')
-if [ -n "ex_cpu" ] || [ -n "$ex_mem" ]; then 
+if [ -n "$ex_cpu_proc" ] || [ -n "$ex_cpu_proc" ]; then 
 	echo "Warning! Extremelly high load!"
 	for pid in ${ex_cpu[@]} ${ex_mem[@]}; do
 		echo "Killing process $pid"
