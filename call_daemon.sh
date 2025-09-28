8c56ff10b354fe50f3a3977ca7f83037263c0b14
UnknownFinder <zuzinma2009@mail.ru>
2025-09-28 16:12:26 +0300

    Update call_daemon.sh

--- call_daemon.sh
+++ call_daemon.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-echo "Enter % of CPU usage"
-read max_cpu
-echo "Enter % of RAM usage"
-read max_ram
-export $max_cpu
-export $max_ram
-echo "You have entered parameters. Please, wait... "
 SERVICE_NAME="hitman"
-EXEC_FILE="${SCRIPT_DIRECTORY}/hitman2.sh $max_cpu $max_ram"
+EXEC_FILE="${SCRIPT_DIRECTORY}/hitman2.sh"
 UNIT_FILE="/etc/systemd/system/hitman.service"
 echo "[Unit]" > $UNIT_FILE
 echo "Description=Daemon to avoid hardware crushs" >> $UNIT_FILE
@@ -15,8 +8,8 @@ echo "After=network.service" >> $UNIT_FILE
 
 echo "[Service]" >> $UNIT_FILE
 echo "Type=exec" >> $UNIT_FILE
-echo "ExecStart="$SCRIPT_DIRECTORY/hitman2.sh $max_cpu $max_ram" >> $UNIT_FILE
-echo "Restart=on-failure" >> $UNIT_FILE
+echo "ExecStart="$SCRIPT_DIRECTORY/hitman2.sh"" >> $UNIT_FILE
+echo "Restart=always" >> $UNIT_FILE
 
 echo "[Install]" >> $UNIT_FILE
 echo "WantedBy=multi-user.target" >> $UNIT_FILE
