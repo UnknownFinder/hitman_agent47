95bafafc85b624791088fe41400f74ce05ab6e99
UnknownFinder <zuzinma2009@mail.ru>
2025-09-28 16:09:00 +0300

    Update find_targets.sh

--- find_targets.sh
+++ find_targets.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-for PORT in ${PORTS[*]}; do
-	grep "New connections from" $watching_from | grep "$PORT" | awk '{print $NF}' | sort -u >> $target_list
-done
-while IFS =read -r ip; do
-	echo "Scanning target $ip" >> $information
-	nmap -A -sV >> $information
-	if [[ $ip =~ .*\.local|.*\.lan ]]; then
-		echo "Getting more information about target $ip" >> $information
-		enum4linux -A $ip >> $information
-	fi
-done < "$target_list"
 
